ad0d33c5180daf669a5688fc9b5740371e23f5b3
TuanKha0904 <94231162+TuanKha0904@users.noreply.github.com>
2024-05-14 15:35:06 +0700

    feat: program 5 - TicTacToe

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -23,8 +23,7 @@ public class Main {
                     break;
                 }
                 case 5: {
-                    System.out.println("TicTacToe");
-                    System.out.println();
+                    TicTacToe();
                     break;
                 }
                 case 6: {
@@ -152,5 +151,102 @@ public class Main {
         for (int i = 0; i < 10; i++) {
             System.out.println("Person " + array[i][0] + " ate " + array[i][1] + " pancakes");
         }
+    }
+    // 5. TicTacToe
+    static void TicTacToe() {
+        String[] board;
+        String turn;
+        Scanner in = new Scanner(System.in);
+        board = new String[9];
+        turn = "X";
+        String winner = null;
+        for (int a = 0; a < 9; a++) {
+            board[a] = String.valueOf(a + 1);
+        }
+        System.out.println("Welcome to 3x3 Tic Tac Toe.");
+        printBoard(board);
+        System.out.println("X will play first. Enter a slot number to place X in:");
+        while (winner == null) {
+            int numInput;
+            try {
+                numInput = in.nextInt();
+                if (!(numInput > 0 && numInput <= 9)) {
+                    System.out.println(
+                            "Invalid input; re-enter slot number:");
+                    continue;
+                }
+            } catch (InputMismatchException e) {
+                System.out.println(
+                        "Invalid input; re-enter slot number:");
+                continue;
+            }
+            if (board[numInput - 1].equals(
+                    String.valueOf(numInput))) {
+                board[numInput - 1] = turn;
+                if (turn.equals("X")) {
+                    turn = "O";
+                } else {
+                    turn = "X";
+                }
+                printBoard(board);
+                winner = checkWinner(board, turn);
+            } else {
+                System.out.println("Slot already taken; re-enter slot number:");
+            }
+        }
+        if (winner.equalsIgnoreCase("draw")) {
+            System.out.println("It's a draw! Thanks for playing.");
+        }
+        else {
+            System.out.println("Congratulations! " + winner + "'s have won! Thanks for playing.");
+        }
+        in.close();
+    }
+    static String checkWinner(String[] board, String turn) {
+        for (int a = 0; a < 8; a++) {
+            String line = switch (a) {
+                case 0 -> board[0] + board[1] + board[2];
+                case 1 -> board[3] + board[4] + board[5];
+                case 2 -> board[6] + board[7] + board[8];
+                case 3 -> board[0] + board[3] + board[6];
+                case 4 -> board[1] + board[4] + board[7];
+                case 5 -> board[2] + board[5] + board[8];
+                case 6 -> board[0] + board[4] + board[8];
+                case 7 -> board[2] + board[4] + board[6];
+                default -> null;
+            };
+            if (line.equals("XXX")) {
+                return "X";
+            }
+            else if (line.equals("OOO")) {
+                return "O";
+            }
+        }
+        for (int a = 0; a < 9; a++) {
+            if (Arrays.asList(board).contains(
+                    String.valueOf(a + 1))) {
+                break;
+            }
+            else if (a == 8) {
+                return "draw";
+            }
+        }
+        System.out.println(turn + "'s turn; enter a slot number to place " + turn + " in:");
+        return null;
+    }
+    static void printBoard(String[] board) {
+        System.out.println("|---|---|---|");
+        System.out.println("| " + board[0] + " | "
+                + board[1] + " | " + board[2]
+                + " |");
+        System.out.println("|-----------|");
+        System.out.println("| " + board[3] + " | "
+                + board[4] + " | " + board[5]
+                + " |");
+        System.out.println("|-----------|");
+        System.out.println("| " + board[6] + " | "
+                + board[7] + " | " + board[8]
+                + " |");
+        System.out.println("|---|---|---|");
     }
 }
