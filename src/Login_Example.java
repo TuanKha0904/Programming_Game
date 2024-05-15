3fccdf1ce8a7d526a33093178c0512b51aa109be
TuanKha0904 <94231162+TuanKha0904@users.noreply.github.com>
2024-05-15 08:24:40 +0700

    feat: login example

---
+++ src/Login_Example.java
@@ -0,0 +1,41 @@
+import javax.swing.*;
+
+public class Login_Example {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Login Example");
+        frame.setSize(300, 150);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JPanel panel = new JPanel();
+        frame.add(panel);
+        placeComponents(panel);
+        frame.setVisible(true);
+    }
+
+    public static void placeComponents(JPanel panel) {
+        panel.setLayout(null);
+
+        JLabel userLabel = new JLabel("User");
+        userLabel.setBounds(10, 20, 80, 25);
+        panel.add(userLabel);
+
+        JTextField userText = new JTextField(20);
+        userText.setBounds(100, 20, 165, 25);
+        panel.add(userText);
+
+        JLabel passwordLabel = new JLabel("Password");
+        passwordLabel.setBounds(10, 50, 80, 25);
+        panel.add(passwordLabel);
+
+        JPasswordField passwordText = new JPasswordField(20);
+        passwordText.setBounds(100, 50, 165, 25);
+        panel.add(passwordText);
+
+        JButton loginButton = new JButton("Login");
+        loginButton.setBounds(10, 80, 80, 25);
+        panel.add(loginButton);
+
+        JButton registerButton = new JButton("Register");
+        registerButton.setBounds(165, 80, 100, 25);
+        panel.add(registerButton);
+    }
+}
